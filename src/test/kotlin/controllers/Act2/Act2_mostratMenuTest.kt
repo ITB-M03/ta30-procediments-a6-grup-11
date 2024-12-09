8391be291ba5812cade156ac7622507fd64f1057
Instituto <anmolpreet.singh.7e8@itb.cat>
2024-12-09 21:58:49 +0100

    Final todo

---
+++ src/test/kotlin/controllers/Act2/Act2_mostratMenuTest.kt
@@ -0,0 +1,13 @@
+package controllers.Act2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class Act2_mostratMenuTest {
+
+@Test
+//no se puede usar la entrada del teclado para este test asi que no se puede hacer
+ fun mostrarMenuTest() {
+  val esperat = 1
+ }
+}
---
+++ src/test/kotlin/controllers/Act2/Act2_vaciaSalaTest.kt
@@ -0,0 +1,13 @@
+package controllers.Act2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class Act2_vaciaSalaTest {
+
+@Test
+//no se pueden hacer tests de las funciones porque no devuelven ningun valor
+ fun vaciaSala() {
+  val esperat = 1
+ }
+}
